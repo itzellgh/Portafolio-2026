4ed4bd296321d398c7f5d3dc2ff75cdb3fbe09a5
itzellgh <lindaitzelgh@gmail.com>
2026-04-24 17:32:04 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function toggleGallery(id) {
+  const el = document.getElementById(id);
+  el.classList.toggle('show');
+}
+
+const images = document.querySelectorAll(".extra-gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.classList.add("show");
+    lightboxImg.src = img.src;
+  });
+});
+
+function closeLightbox() {
+  lightbox.classList.remove("show");
+}
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== lightboxImg) {
+    closeLightbox();
+  }
+});
+
+const serviceID = 'service_5lwfcg8';
+const templateID = 'template_zdwrqjr';
+const userID = 'T5dpphxYzcgF6kUGr';
+emailjs.init('service_5lwfcg8');
+
+
+document.getElementById('form-contact').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    emailjs.sendForm(serviceID, templateID, this)
+    .then(function() {
+        alert('Mensaje enviado con éxito!');
+    }, function(error) {
+        alert('Error: ' + JSON.stringify(error));
+    });
+});
